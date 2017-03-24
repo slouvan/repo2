112ff184eb686c77a9ef8ec412c7be66292b7f17
Samuel Louvan <samuel.louvan@gmail.com>
2017-03-24 13:45:58 +0700

    Menambahkan setter

--- Student.java
+++ Student.java
@@ -2,4 +2,14 @@ public class Student
 {
 	private String name;
 	private double gpa;
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public void setGpa(double gpa)
+	{
+		this.gpa = gpa;
+	}
 }
